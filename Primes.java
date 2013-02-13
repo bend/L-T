8b1da69921904f825110665845152a6f4ce284fe
Ben D <ben.daccache@gmail.com>
2013-02-13 18:50:40 +0100

    Fixed compile

--- Primes.java
+++ Primes.java
@@ -1,4 +1,5 @@
 import java.lang.System;
+import java.lang.Integer;
 
 public class Primes {
     private int[] sieve;
@@ -44,75 +45,13 @@ public class Primes {
         System.out.println("");
     }
 
-    public static int toInt(String str) {
-        int len = str.length();
-        int pos = 0;
-        int res = 0;
-        while (len > pos) {
-            if (str.charAt(pos) == '0') 
-            {
-                res = res* 10;
-            }
-            else if (str.charAt(pos) == '1') 
-            {
-                res = res* 10;
-                res+= 1;
-            }
-            else if (str.charAt(pos) == '2') 
-            {
-                res = res* 10;
-                res+= 2;
-            }
-            else if (str.charAt(pos) == '3') 
-            {
-                res = res* 10;
-                res+= 3;
-            }
-            else if (str.charAt(pos) == '4') 
-            {
-                res = res* 10;
-                res+= 4;
-            }
-            else if (str.charAt(pos) == '5') 
-            {
-                res = res* 10;
-                res+= 5;
-            }
-            else if (str.charAt(pos) == '6') 
-            {
-                res = res* 10;
-                res+= 6;
-            }
-            else if (str.charAt(pos) == '7') 
-            {
-                res = res* 10;
-                res+= 7;
-            }
-            else if (str.charAt(pos) == '8') 
-            {
-                res = res* 10;
-                res+= 8;
-            }
-            else if (str.charAt(pos) == '9') 
-            {
-                res = res* 10;
-                res+= 9;
-            }
-            else 
-                return 0;
-            
-            pos+=1;
-        }
-        return res;
-    }
-
 
     public static void main(String[] args) {
-        if (args.length() == 0) {
+        if (args.length == 0) {
             System.out.println("Missing argument");
             return;
         }
-        int i = Primes.toInt(args[0]);
+        int i = Integer.parseInt(args[0]);
         Primes primes = new Primes(i);
         primes.erastosthene(); 
     }
