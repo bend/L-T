6a746090aa0d045eacaacb857ff78011fc530348
Ben D <ben.daccache@gmail.com>
2013-03-14 09:48:33 +0100

    Updated gitignore

--- j--/tests/junit/JMinusMinusTestRunner.java
+++ j--/tests/junit/JMinusMinusTestRunner.java
@@ -21,7 +21,6 @@ public class JMinusMinusTestRunner {
         suite.addTestSuite(GCDTest.class);
         suite.addTestSuite(SeriesTest.class);
         suite.addTestSuite(ClassesTest.class);
-        suite.addTestSuite(DivisionTest.class);
         return suite;
     }
 
